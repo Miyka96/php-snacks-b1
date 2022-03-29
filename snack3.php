43af9ff33a6b85bce3148774ebe065518ccabb6b
Miyka96 <micaelaseminara@outlook.it>
2022-03-29 18:27:14 +0200

    snack 3 - array

--- snack3.php
+++ snack3.php
@@ -4,6 +4,52 @@
 // DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni
 // data con i relativi post.
 // Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
-    
+
+$posts[
+    '29/03/2022'=>
+    [
+        ['title'=> 'Il legno è vivo',
+        'author' => 'Giovannino Pane&Vino',
+        'text' => 'Anatomia di uno sgabello'
+        ],
+        ['title'=> 'Le cose',
+        'author' => 'Francesco PopiPopi',
+        'text' => 'Interpretazione della fisiognomica dei clacson'
+        ],
+    ],
+    '23/02/2022'=>
+    [
+        ['title'=> 'Martha ha tre bambini',
+        'author' => 'Luca Pavid',
+        'text' => 'Interpretare gli ultrasuoni dei neonati'
+        ],
+        ['title'=> 'Il montaggio',
+        'author' => 'Ivo Avid',
+        'text' => 'Quattro chiodi cambiano il mondo'
+        ],
+    ],
+    '20/02/2022'=>
+    [
+        ['title'=> 'Il codice',
+        'author' => 'Leonardo Da Vinci',
+        'text' => 'In realtà è stato scritto a casaccio'
+        ],
+        ['title'=> 'La verità dietro al velo',
+        'author' => 'Pragmatanga Yogapanza',
+        'text' => 'Per svelare la realtà dietro al velo di Maya, basta spogliare Maya'
+        ],
+    ],
+    '29/03/2022'=>
+    [
+        ['title'=> 'I ravanelli sono malvagi',
+        'author' => 'Sconosciuto',
+        'text' => 'Un avvertimento che può salvarvi la vita'
+        ],
+        ['title'=> 'Io sono Giovanna',
+        'author' => 'Giovanna',
+        'text' => 'Sono una madre'
+        ],
+    ],
+];
 
 ?>
