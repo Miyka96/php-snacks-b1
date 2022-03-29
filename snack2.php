f7148efc3bbf1f687314e841e23042f48021d02d
Miyka96 <micaelaseminara@outlook.it>
2022-03-29 17:59:32 +0200

    snack 2

--- snack2.php
+++ snack2.php
@@ -5,5 +5,26 @@
 // chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso
 // negato”
 
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
+$_GET=["name"];
+$_GET=["mail"];
+$_GET=["age"];
+$pos_dot = strpos($mail, '.', -4);
+$pos_at = strpos($mail, '@');
+$len_name = strlen($name);
 
-?>
+// var_dump($len_name);
+// var_dump($pos_at);
+// var_dump($pos_dot);
+
+
+    if($len_name >3 && $pos_dot && $pos_at && is_numeric($age)){
+        echo('Accesso riuscito');
+    }
+    else{
+        echo('Accesso negato');
+    }
+        
+
